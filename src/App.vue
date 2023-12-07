edfe118b12fcb7e30957ccbaf8ff8eaa49b7dec5
DroidStudioAS <as.droiddevteam@gmail.com>
2023-12-07 11:04:20 +0100

    restyled homed page

--- src/App.vue
+++ src/App.vue
@@ -1,114 +1,134 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
 
 <template>
 <div class="main_container">
-      <div class="nav_container">
-          <div class="nav_item">About Me</div>
-          <div class="nav_item">Skills</div>
-          <div class="nav_item">Projects</div>
-          <div class="nav_item">Contact</div>
-          <div class="menu_icon">Menu</div>
-      </div>
-
-      <div class="content_container">
-        <div class="header_container">
-        
-            <h2 class="name">Aleksandar Smiljanic</h2>
-            <h1 class="title">Software Developer</h1>
-        </div>
-        <div class="icon_container">
-            <img class="icon_landing" src="/icon_landing.png">
-        </div>
-      </div>
-    </div>
+  <div class="header_container">
+      <h1 class="title">{{ title }}</h1>
+      <h1 class="title_end">{{ titleend }}</h1>
+      <h2 class="name">{{ name }}</h2>
+  </div>
+  <div class="image_container">
+    <img class="home_image" src="/icon_landing_no_text.png">
+  </div>
+</div>
+<div class="navigation_container">
+  <div class="menu_but">{{ menu }}</div>
+</div>
 
 </template>
+<script setup>
+import { ref } from 'vue';
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import {gsap} from"gsap";
+import { onMounted } from 'vue';
 
+let menu = ref("menu")
+let name=ref("Aleksandar Smiljanic")
+let title = ref("Software")
+let titleend=ref("Developer")
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+</script>
 <style scoped>
-.main_container{
-  height: 100vh;
-  width: 100vw;
-  background-image: linear-gradient(90deg, #cdffd8, #94b9ff);
-  position: absolute;
-  top: 0%;
-  left: 0%;
-  right: 0%;
+*{
+  overflow-y: hidden;
+  overflow-x: hidden;
 }
-.menu_icon{
-  margin-right: 4%;
-}
-
-.icon_container{
- position: absolute;
- right: 0vw;
- height: 80vh;
- width: 40vw;
-
-}
-.icon_landing{
+.home_image{
   height: 80vh;
- width: 40vw;
- border-radius: 5%;
 }
-.content_container{
-  display: flex;
-  flex-flow: row nowrap;
+.image_container{
   position: absolute;
-  top:20vh ;
-  left: 0vw;
+  bottom: -1vh;
+  right: 10vw;
   height: 80vh;
-  width: 100vw;
-  color: black;
+  
+}
+
+.title{
+  font-size: 62px;
+  color: #000;
+  font-weight: 900;
 
 }
-.header_container{
-  display: flex;
-  flex-flow: column nowrap;
-  width: 60vw;
-  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
-  justify-content: center;
-  align-items: start;
-  margin-bottom: 20%;
-  margin-left: 2%;
+.title_end{
+  font-size: 62px;
+  color: #000;
+  font-weight: 900;
+  margin-top: -20%;
+
 }
 .name{
-  font-size: 7cap;
-  font-weight: 100;
+  font-size: 40px;
+  color: #000;
+  font-weight: 300;
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
 }
-.title{
-  font-size: 8cap;
-  font-weight: bold;
-}
-.nav_container{
+.header_container{
+  
   display: flex;
-  flex-flow: row nowrap;
+  flex-flow: column nowrap;
+  gap: 5vh;
+  align-content: center;
+  justify-content: center;
+  text-align: center;
+
+  position: absolute;
+  top: 10vh;
+  left: 20vw;
+}
+.main_container{
+  background-color: #89B4BE;
+
   position: absolute;
   top: 0%;
   left: 0%;
-  width: 100vw;
-  height: 20vh;
-  justify-content: start;
-  margin-left: 3%;
-  gap: 15%;
-  align-items: center;
-  color: black;
+  bottom: 0%;
+  height: 100vh;
+  width: 90vw;
 }
-.nav_item{
-  background-image: url("/background_nav_item.png");
-  width: 100px;
-  height: 60px;
-  display: flex;
-  justify-content: center ;
-  align-items: center;
-  font-size: large;
-  font-weight: bold;
-}
-.menu_icon{
-  position: absolute;
-  right: 2%;
-  justify-self: right;
-}
+  .navigation_container{
+    background-color: #4E7896;
+
+    position: absolute;
+    top: 0%;
+    right: 0%;
+    bottom: 0%;
+
+    height: 100vh;
+    width: 10vw;
+
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: center;
+  }
+  .menu_but{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+   
+    margin: 5%;
+
+    border-radius: 30%;
+
+    background-color: #FAEFE6;
+    color:#87A1B0;
+    font-size: 300%;    
+    height: 7vh;
+    width: 9vw;
+
+    padding: 20% 10% 25% 10%
+  }
 </style>
