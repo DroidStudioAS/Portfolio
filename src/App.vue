7f59d7c45a6de0b2923a1602e19ecab91474e227
DroidStudioAS <as.droiddevteam@gmail.com>
2023-12-07 07:33:22 +0100

    home page structure setup

--- src/App.vue
+++ src/App.vue
@@ -4,69 +4,101 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  
+<div class="main_container">
+      <div class="nav_container">
+          <div class="nav_item">About Me</div>
+          <div class="nav_item">Skills</div>
+          <div class="nav_item">Projects</div>
+          <div class="nav_item">Contact</div>
+          <div class="menu_icon">Menu</div>
+      </div>
+
+      <div class="content_container">
+        <div class="header_container">
+        
+            <h2 class="name">Aleksandar Smiljanic</h2>
+            <h1 class="title">Software Developer</h1>
+        </div>
+        <div class="icon_container">
+            <img class="icon_landing" src="/icon_landing.png">
+        </div>
+      </div>
+    </div>
+
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.main_container{
+  height: 100vh;
+  width: 100vw;
+  background-image: linear-gradient(90deg, #cdffd8, #94b9ff);
+  position: absolute;
+  top: 0%;
+  left: 0%;
+  right: 0%;
+}
+.menu_icon{
+  margin-right: 4%;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.icon_container{
+ position: absolute;
+ right: 0vw;
+ height: 80vh;
+ width: 40vw;
+
 }
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+.icon_landing{
+  height: 80vh;
+ width: 40vw;
+ border-radius: 5%;
 }
+.content_container{
+  display: flex;
+  flex-flow: row nowrap;
+  position: absolute;
+  top:20vh ;
+  left: 0vw;
+  height: 80vh;
+  width: 100vw;
+  color: black;
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
 }
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.header_container{
+  display: flex;
+  flex-flow: column nowrap;
+  width: 60vw;
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  justify-content: center;
+  align-items: start;
+  margin-bottom: 20%;
+  margin-left: 2%;
 }
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+.name{
+  font-size: 7cap;
+  font-weight: 100;
 }
-
-nav a:first-of-type {
-  border: 0;
+.title{
+  font-size: 8cap;
+  font-weight: bold;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.nav_container{
+  display: flex;
+  flex-flow: row nowrap;
+  position: absolute;
+  top: 0%;
+  left: 0%;
+  width: 100vw;
+  height: 20vh;
+  justify-content: start;
+  margin-left: 3%;
+  gap: 20%;
+  align-items: center;
+  color: black;
+}
+.menu_icon{
+  position: absolute;
+  right: 2%;
+  justify-self: right;
 }
 </style>
