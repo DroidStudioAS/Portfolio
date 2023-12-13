166c78a4fbccedcc464a78d7e5c40cd9a64a5e01
DroidStudioAS <as.droiddevteam@gmail.com>
2023-12-13 08:23:25 +0100

    added mock project component to projectview and added toggling for project click (showPc) and for close button click (closePc)

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -3,6 +3,15 @@ import { onMounted, ref } from 'vue';
 import project from '../models/project';
 
 let headerColor = ref("#000");
+let close_pc_button = ref(null);
+let pc = ref(null);
+
+function closePcContainer(){
+    pc.value.style.visibility = 'hidden';
+}
+function showPc(){
+    pc.value.style.visibility = 'visible';
+}
 const projects = [
     new project("Fit Tracker",false),
     new project('Running Tracker',true),
@@ -17,7 +26,7 @@ const projects = [
 ];
 
 onMounted(()=>{
-    headerColor.value="#000"
+
 })
 
 
@@ -27,16 +36,19 @@ onMounted(()=>{
         Projects
     </h1>
     <div class="main_container">
-        <div v-for="project in projects" :key="project.name" >
+        <div v-for="project in projects" 
+        :key="project.name" >
         <div v-if="project.even===true"
-        class="project_image_containet">
+        class="project_image_containet"
+        @click="showPc()">
             <img src="/icon_folder_even.png">
             <div class="project_label">
              {{ project.name }}
              </div>
         </div>
         <div v-else
-        class="project_image_containet">           
+        class="project_image_containet"
+        @click="showPc()">           
          <img src="/icon_folder_uneven.png">
          <div class="project_label">
         {{ project.name }}
@@ -44,12 +56,47 @@ onMounted(()=>{
         </div>
        
 
-    </div>
+        </div>
+        <div ref="pc" class="project_component">
+            <div  ref="close_pc_button" 
+            class="close_pc_container"
+            @click="closePcContainer()" >
+
+            </div>
+            <div  class="project_name_container">
+                <h1>
+                    Lorem Ipsum
+                </h1>
+            </div>
+            <div class="project_type_container">
+                <h2>
+                     Ipsum Lorem
+                </h2>
+            </div>
+            <div class="project_logo_container">
+
+            </div>
+            <div class="project_description_container">
+                <p>
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.""
+
+                </p>
+            </div>
+            <div class="project_links_container">
+                <div>image</div>
+                <div>image</div>
+                <div>image</div>
+            </div> 
+        </div>
 </div>
 
 </template>
 <style scoped>
-
+*{
+    color: white;
+}
+/************CRUCIAL START*************/
 .main_container{
     height: 80vh;
     width: 100vw;
@@ -104,6 +151,82 @@ width: 20vw;
 
     color: white;
 }
+ /********project_component start*********/
+.project_component{
+    background: #4E7896;
+    width: 45vw;
+    height: 50vh;
+    position: fixed;
+    left: 30vw;
+
+    display: flex;
+    flex-flow: column nowrap;
+
+    border-radius: 15px;
+
+}
+/********pc_layout_items*******/
+/***50vh and 45vw is the space the children are working with***/
+/** 
+-10vh projectname (40 leftt)
+-8vh projecttype (32 left)
+-20vh description (12 left)
+*/
+.close_pc_container{
+    width: 5vw;
+    height: 8vh;
+    background: paleturquoise;
+
+    position: absolute;
+    right: 0;
+
+    border-radius: 15px;
+    
+}
+.project_name_container{
+    width: 45vw;
+    height: 10vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 130%;
+}
+.project_type_container{
+    height: 8vh;
+    width: 45vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 130%;
+
+}
+.project_description_container{
+    width: 40vw;
+    height: 20vh;
+    position: relative;
+    left: 2.5vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.project_links_container{
+    height: 12vh;
+    background: pink;
+    width: 45vw;
+
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-around;
+    align-items: center;
+}
+  /********project_component end*********/
+
+
+/************Crucial End*************/
+
+
+
+/*******************Media Queries Start********************/
 @media (min-width:767px) and (max-width:991px){
     .main_container{
         width: 100vw;
@@ -132,4 +255,6 @@ width: 20vw;
         width: 20vw;
     }
 }
+/*******************Media Queries End********************/
+
 </style>
